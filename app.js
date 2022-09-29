f54d42659cbac8555a0ab2da3d9fd3fb8ac2ca3b
shubhankarpatil <47353671+shubhankarpatil@users.noreply.github.com>
2022-09-29 12:10:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-// app.use(express.static("public"));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
